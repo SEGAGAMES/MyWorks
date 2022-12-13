2deaecf56bba3b7c83fec4a807f277979f3722b0
Святослав <svyatkh@gmail.com>
2022-12-13 17:44:46 +1000

    Server1.0

--- 8/Program.cs
+++ 8/Program.cs
@@ -1,10 +1,38 @@
-﻿namespace _8
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Сервер_удалённого_доступа
 {
     internal class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Дадоваf");
-        }
+        static int port = 2022;
+        
+            static IPEndPoint ipPoint = new IPEndPoint(IPAddress.Any, port);
+            static Socket listenSocket =
+            new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            static void Main()
+            {
+                try
+                {
+                    listenSocket.Bind(ipPoint);
+                    listenSocket.Listen(10);
+                    do
+                    {
+                        Socket handler = listenSocket.Accept();
+                        Console.WriteLine($"Входящее подключение от {handler.RemoteEndPoint}");
+                        // Создание объекта для работы с соединением в отдельном потоке
+                    } while (true);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            } 
     }
 }
---
+++ 8/WorkWithClient.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    internal class WorkWithClient
+    {
+        static int count = 0;
+        Socket socket;
+        int id;
+        string clientInfo;
+        public WorkWithClient(Socket socket)
+        {
+            this.socket = socket;
+            id = count;
+            count++;
+            clientInfo = socket.RemoteEndPoint.ToString();
+        }
+
+        string answer;
+        StringBuilder builder = new StringBuilder();
+        byte[] data;
+        int dataLength;
+        public void Run()
+        {
+            Console.WriteLine($"Client №{id}. Информация: Установлено соединение с \"{clientInfo}\"");
+            try
+            {
+                do
+                {
+                    // Получение команды
+                    data = new byte[256];
+                    builder.Clear();
+                    do
+                    {
+                        dataLength = socket.Receive(data);
+                        builder.Append(Encoding.Unicode.GetString(data, 0, dataLength));
+                    } while (socket.Available > 0);
+                    Console.WriteLine($"Client №{id}. Команда: {builder.ToString()}");
+                    // Обработка команды для генерации ответа
+                    answer = DateTime.Now.ToString();
+                    // Отправка ответа клиенту
+                    data = Encoding.Unicode.GetBytes(answer);
+                    socket.Send(data);
+                } while (builder.ToString() != "exit");
+                Console.WriteLine($"Client №{id}. Информация: Клиент отключился");
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine($"Client №{id}. Ошибка: {e.Message}");
+            }
+        }
+    }
+}
